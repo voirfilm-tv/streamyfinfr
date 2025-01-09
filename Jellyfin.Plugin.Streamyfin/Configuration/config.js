46d78abc57fc9e1b1ea6b2ee910a370917cafeef
herrrta <73949927+herrrta@users.noreply.github.com>
2025-01-09 08:47:22 -0500

    App Settings configuration for Streamyfin (#1)

---
+++ Jellyfin.Plugin.Streamyfin/Configuration/config.js
@@ -0,0 +1,99 @@
+export default function (view) {
+    const Streamyfin = {
+        pluginId: "1e9e5d38-6e67-4615-8719-e98a5c34f004",
+        btnSave: document.querySelector("#saveConfig"),
+        editor: null,
+        saveConfig: function (e) {
+            e.preventDefault();
+            Dashboard.showLoadingMsg();
+            const config = {
+                Value: Streamyfin.editor.getModel().getValue()
+            };
+
+            const url = window.ApiClient.getUrl('streamyfin/config/yaml');
+            const data = JSON.stringify(config);
+
+            window.ApiClient.ajax({ type: 'POST', url, data, contentType: 'application/json' })
+                .then(function (response) {
+                        response.json().then(res => {
+                            if (res.Error == true) {
+                                Dashboard.hideLoadingMsg();
+                                Dashboard.alert(res.Message);
+                            } else {
+                                Dashboard.processPluginConfigurationUpdateResult();
+                            }
+                        })
+                    }
+
+                )
+                .catch(function (error) {
+                    //alert(error);
+                    console.error(error);
+                })
+                .finally(function () {
+                    Dashboard.hideLoadingMsg();
+                });
+
+        },
+        loadConfig: function () {
+            Dashboard.showLoadingMsg();
+            const url = window.ApiClient.getUrl('streamyfin/config/yaml');
+            window.ApiClient.ajax({ type: 'GET', url, contentType: 'application/json' })
+                .then(function (response) {
+                    response.json().then(res => {
+                        const yamlModelUri = monaco.Uri.parse('streamyfin.yaml');
+                        Streamyfin.editor = monaco.editor.create(document.getElementById('yamleditor'), {
+                            automaticLayout: true,
+                            language: 'yaml',
+                            quickSuggestions: {
+                                other: true,
+                                comments: false,
+                                strings: true
+                            },
+                            model: monaco.editor.createModel(res.Value, 'yaml', yamlModelUri),
+                        });
+                    })
+                })
+                .catch(function (error) {
+                    console.error(error);
+                })
+                .finally(function () {
+                    Dashboard.hideLoadingMsg();
+                });
+        },
+        init: function () {
+            monaco.editor.setTheme('vs-dark');
+            const monaco_yaml = monacoYaml.configureMonacoYaml(monaco, {
+                enableSchemaRequest: true,
+                hover: true,
+                completion: true,
+                validate: true,
+                format: true,
+                schemas: [
+                    {
+                        uri: location.origin + '/streamyfin/config/schema',
+                        fileMatch: ["*"],
+                    },
+                ],
+            });
+            console.log("init");
+            Streamyfin.loadConfig();
+            Streamyfin.btnSave.addEventListener("click", Streamyfin.saveConfig);
+        }
+    }
+
+    view.addEventListener("viewshow", function (e) {
+        waitForScript();
+    });
+
+    function waitForScript() {
+        if (typeof monaco === "undefined") {
+            setTimeout(waitForScript, 50);
+        } else {
+            Streamyfin.init();
+        }
+    }
+}
+
+
+// waitForScript();
--- README.md
+++ README.md
@@ -5,17 +5,58 @@ Allows centralised configuration of Streamyfin.
 Config example:
 
 ```yaml
-home:
-  sections:
-    Trending:
-      style: portrait
-      type: row 
-      items:
-        args: 
-          sortBy: AddedDate
-          sortOrder: Ascending
-          filterByGenre: [""Anime"", ""Comics""]";
+# You can remove any settings you do not need configured.
+
+# Format Example
+# settingName:
+#   locked: true | false # if true, locks the setting from modification in app. Default false.
+#   value: value # Value you want the setting to be. Editor will give you type suggestion for a specific setting.
+
+# Example below shows all supported settings at this time.
+settings:
+  # Media Controls
+  forwardSkipTime:
+  rewindSkipTime: 
+
+  # Audio Controls
+  rememberAudioSelections:
+  
+  # Subtitles
+  subtitleMode:
+  rememberSubtitleSelections:
+  subtitleSize:
+  
+  # Other
+  autoRotate:
+  defaultVideoOrientation:
+  safeAreaInControlsEnabled:
+  showCustomMenuLinks:
+  hiddenLibraries:
+  disableHapticFeedback:
+  
+  # Downloads
+  downloadMethod:
+  remuxConcurrentLimit:
+  autoDownload:
+  optimizedVersionsServerUrl:
+
+  # Jellyseerr 
+  jellyseerrServerUrl:
+  
+  # Search
+  searchEngine:
+  marlinServerUrl:
+
+  # Popular Lists
+  usePopularPlugin:
+  mediaListCollectionIds:
+
+  # Misc.
+  libraryOptions:
 ```
+
+#### Supported Streamyfin App configurations
+[Settings.cs](Jellyfin.Plugin.Streamyfin/Configuration/Settings/Settings.cs)
 
 repo url: https://raw.githubusercontent.com/streamyfin/jellyfin-plugin-streamyfin/main/manifest.json
 
